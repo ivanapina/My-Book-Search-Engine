cec9bb9117cd9597b4bf0266be75b62257b8597b
ivanapina <ivanapina@gmail.com>
2023-10-03 21:12:40 -0500

    installed missing packages

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { expressMiddleware } = require('@apollo/server/express4');
 
 // Importing our typeDefs & resolvers objects
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
